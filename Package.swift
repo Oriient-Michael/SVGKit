5d94b1e1d738f953b731d96e6461c9519c144df8
Michael <michaelk@oriient.me>
2025-01-23 11:12:23 +0200

    Adjusted package

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
             exclude: [
                 "SwiftUI additions"
             ],
-            resources: [.process("Resources/PrivacyInfo.xcprivacy")],
+            resources: [],
             publicHeadersPath: "include",
             cSettings: [
                 .headerSearchPath("privateHeaders"),
